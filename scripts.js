da9dbbabe5598398089d9198c60c1f814a5ad0c0
csdaniel88 <40003687+csdaniel88@users.noreply.github.com>
2020-04-23 17:06:56 +0200

    second

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 let name = 'Dani';
 
-name + "'s apples";
-
 console.log(name + "'s apples")
 console.log('Hello World!')
+
+// $('selector').action()
+$('h1').text('We are having a 10 minute break until 17:05');
+
+
+
 
 // This is a Javascript single line comment
 
